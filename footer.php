af83f92988842e984c8cbb633771d52d6455068b
315030020 <37674960+315030020@users.noreply.github.com>
2018-04-10 22:17:24 -0500

    usuarios y stock
    Co-Authored-By: mariopapu <mariopapu@users.noreply.github.com>

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="navbar navbar-default navbar-fixed-bottom" >
+    <div class="container">
+      <p class="navbar-text pull-left">&copy <?php echo date('Y');?> - Botines el Chilango.
+           <a href="http://ite-tavo.000webhostapp.com/index.html" target="_blank" style="color: #DC2525">Sitio Web</a>
+      </p>
+   </div>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
